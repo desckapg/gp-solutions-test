7ccae968d3e7d702960fee2777949d67bf161cc7
Denis Sapezhko <denissapezko748@gmail.com>
2026-04-24 18:10:57 +0300

    FEAT-5: write base JPA entities

---
+++ src/main/java/com/gpsolutions/hotels/domain/entity/Address.java
@@ -0,0 +1,71 @@
+package com.gpsolutions.hotels.domain.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToOne;
+import jakarta.persistence.Table;
+import java.util.Objects;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.hibernate.proxy.HibernateProxy;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@Entity
+@Table(name = "addresses")
+public class Address extends BaseEntity<Long> {
+
+  @Column(name = "house_number")
+  private String houseNumber;
+
+  @Column(name = "street")
+  private String street;
+
+  @Column(name = "city")
+  private String city;
+
+  @Column(name = "country")
+  private String country;
+
+  @Column(name = "postal_code")
+  private String postalCode;
+
+  @OneToOne(fetch = FetchType.LAZY)
+  @JoinColumn(name = "hotel_id")
+  private Hotel hotel;
+
+  @Override
+  public final boolean equals(Object o) {
+    if (this == o) {
+      return true;
+    }
+    if (o == null) {
+      return false;
+    }
+    Class<?> oEffectiveClass =
+        o instanceof HibernateProxy proxy ? proxy.getHibernateLazyInitializer()
+                                            .getPersistentClass() : o.getClass();
+    Class<?> thisEffectiveClass =
+        this instanceof HibernateProxy proxy ? proxy.getHibernateLazyInitializer()
+                                               .getPersistentClass() : this.getClass();
+    if (thisEffectiveClass != oEffectiveClass) {
+      return false;
+    }
+    Address address = (Address) o;
+    return getId() != null && Objects.equals(getId(), address.getId());
+  }
+
+  @Override
+  public final int hashCode() {
+    return this instanceof HibernateProxy proxy ? proxy.getHibernateLazyInitializer()
+                                                  .getPersistentClass().hashCode()
+        : getClass().hashCode();
+  }
+
+}
---
+++ src/main/java/com/gpsolutions/hotels/domain/entity/Amenity.java
@@ -0,0 +1,22 @@
+package com.gpsolutions.hotels.domain.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@Entity
+@Table(name = "amenities")
+public class Amenity extends BaseEntity<Long> {
+
+  @Column(name = "name")
+  private String name;
+
+}
