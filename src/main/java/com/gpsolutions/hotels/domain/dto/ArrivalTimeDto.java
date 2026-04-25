6aaf97ad6dc7d8605bed6f656516e726baf694a1
Denis Sapezhko <denissapezko748@gmail.com>
2026-04-25 21:27:04 +0300

    FIX-17: change checkIn, checkOut format in json serialization

--- src/main/java/com/gpsolutions/hotels/domain/dto/ArrivalTimeDto.java
+++ src/main/java/com/gpsolutions/hotels/domain/dto/ArrivalTimeDto.java
@@ -1,5 +1,6 @@
 package com.gpsolutions.hotels.domain.dto;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import jakarta.validation.constraints.NotNull;
 import java.time.LocalTime;
 
@@ -7,7 +8,10 @@ public record ArrivalTimeDto(
     @NotNull(
         message = "Check in time must be present"
     )
+    @JsonFormat(pattern = "HH:mm")
     LocalTime checkIn,
+
+    @JsonFormat(pattern = "HH:mm")
     LocalTime checkOut
 ) {
 
